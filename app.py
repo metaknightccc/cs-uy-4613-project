37626f93fe17ef0565eceea350031573bfc2e782
metaknightccc <sunyifanc@126.com>
2023-04-09 22:56:58 -0400

    12

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title("Sentiment Analysis App")
 
-user_input = st.text_input("input a sentence to analyze")
+user_input = st.text_input("input a sentence to analyze", "I'm happy")
