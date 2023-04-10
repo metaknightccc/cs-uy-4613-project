4f692986ed071886d1cadd92b135c76e19a4fe78
metaknightccc <sunyifanc@126.com>
2023-04-09 23:07:44 -0400

    12345

--- app.py
+++ app.py
@@ -1,5 +1,43 @@
 import streamlit as st
+from transformers import AutoModelForSequenceClassification
+from transformers import TFAutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoConfig
+import numpy as np
+from scipy.special import softmax
+
+
+def preprocess(text):
+    new_text = []
+    for t in text.split(" "):
+        t = '@user' if t.startswith('@') and len(t) > 1 else t
+        t = 'http' if t.startswith('http') else t
+        new_text.append(t)
+    return " ".join(new_text)
+
+
+MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+config = AutoConfig.from_pretrained(MODEL)
+
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+
+
 
 st.title("Sentiment Analysis App")
 
 user_input = st.text_input("input a sentence to analyze", "I'm happy")
+
+if st.button("Analyze"):
+
+    text = preprocess(user_input)
+    encoded_input = tokenizer(text, return_tensors='pt')
+    output = model(**encoded_input)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+
+    ranking = np.argsort(scores)
+    ranking = ranking[::-1]
+    for i in range(scores.shape[0]):
+        l = config.id2label[ranking[i]]
+        s = scores[ranking[i]]
+        st.write(f"{i + 1}) {l} {np.round(float(s), 4)}")
